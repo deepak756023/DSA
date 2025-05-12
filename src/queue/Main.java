a6f908e84d63aef9478c3fc60ecca71b6ef89c8b
deepak-nayak <deepaknayak55260@gmail.com>
2025-05-12 10:46:28 +0530

    reverse a queue

---
+++ src/queue/Main.java
@@ -0,0 +1,20 @@
+package queue;
+
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        ReverseQueue reverseQueue = new ReverseQueue();
+        Queue<Integer> queue = new ArrayDeque<>();
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+        System.out.println(queue);
+        reverseQueue.reverse(queue);
+
+    }
+
+}
