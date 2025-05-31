a77c83514754721e94be234d02346891cfd32d8c
deepak-nayak <deepaknayak55260@gmail.com>
2025-05-31 09:03:39 +0530

    remove method in HashMap

--- src/hash_table/Main.java
+++ src/hash_table/Main.java
@@ -15,6 +15,7 @@ public class Main {
         table.put(8, "B");
         table.put(11, "C");
         table.put(6, "A+");
+        table.remove(11);
         System.out.println(table.get(6));
 
     }
