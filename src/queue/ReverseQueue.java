311e19f5818e42bec5474f2138e96068f272e7b7
deepak-nayak <deepaknayak55260@gmail.com>
2025-06-05 15:04:10 +0530

    reverse till kth elements
    Given an integer K and a queue of integers, write code to reverse the order of the first K elements of the queue.

--- src/queue/ReverseQueue.java
+++ src/queue/ReverseQueue.java
@@ -1,5 +1,6 @@
 package queue;
 
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -17,4 +18,50 @@ public class ReverseQueue {
 
     }
 
+    // The reverse method more space-efficient and cleaner than reverseKth,while
+    // both correctly reverse the first k elements of the queue in O(n) time.
+
+    public void reverseKthD(Queue<Integer> queue, int k) {
+        Stack<Integer> stack = new Stack<>();
+        LinkedList<Integer> list = new LinkedList<>();
+        int i = 0;
+        while (!queue.isEmpty()) {
+            if (i < k) {
+                stack.push(queue.remove());
+                i++;
+            }
+
+            else
+                list.addLast(queue.remove());
+        }
+        while (!stack.isEmpty())
+            queue.add(stack.pop());
+        while (!list.isEmpty())
+            queue.add(list.removeFirst());
+
+        System.out.println(queue);
+
+    }
+
+    public void reverseKthM(Queue<Integer> queue, int k) {
+        if (k < 0 || k > queue.size())
+            throw new IllegalArgumentException();
+
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i = 0; i < k; i++)
+            stack.push(queue.remove());
+
+        while (!stack.empty())
+            queue.add(stack.pop());
+
+        // Add the remaining items in the queue (items
+        // after the first K elements) to the back of the
+        // queue and remove them from the beginning of the queue
+        for (int i = 0; i < queue.size() - k; i++)
+            queue.add(queue.remove());
+
+        System.out.println(queue);
+
+    }
 }
